0adc6ae6a2d44db43f079fbce81d95696ee1e884
matrunych <matrunych@ucu.edu.ua>
2020-06-29 15:27:57 +0300

    Initial commit

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.d
@@ -0,0 +1,98 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Inc/stm32f4xx_hal_conf.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/cmsis_version.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/mpu_armv7.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ /Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Inc/stm32f4xx_hal_conf.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/cmsis_version.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+/Users/macbookpro/Desktop/STM_Labki/lichylnyk/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
